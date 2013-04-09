33ad86a7be9e99f4b2e3561908efeb0109916cf4
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-04-09 14:08:47 +0300

    Respect back pressure

--- index.js
+++ index.js
@@ -2,19 +2,27 @@ var fs = require("fs");
 var stream = require("stream");
 
 var catFrames = fs.readFileSync(__dirname + "/nyan.data").toString().split("END");
+var clearScreen = "\033[H\033[2J\033[?25l";
 
 module.exports = function() {
     var pt = new stream.PassThrough();
-    pt.nyan = function() {
-        var i = 0;
-        setInterval(function() {
-            pt.write("\033[H\033[2J\033[?25l");
-            pt.write(catFrames[i % catFrames.length]);
-            i++;
-        }, 100);
-        return this;
-    };
+    var i = 0;
 
+    // http://nodejs.org/api/stream.html#stream_writable_write_chunk_encoding_callback_1
+    function nyan() {
+        var frame =  clearScreen + catFrames[i % catFrames.length];
+        i++;
+        if (pt.write(frame)) {
+            setTimeout(nyan, 100);
+        }
+        else {
+            pt.once("drain", nyan);
+        }
+
+        return this;
+    }
+
+    pt.nyan = nyan;
     return pt;
 };
 
